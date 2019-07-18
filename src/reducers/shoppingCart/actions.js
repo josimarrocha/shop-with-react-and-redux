ad1ab0f95c240799c92d4e172ac4b1ece14198f2
Josimar <rocha_josimar@yahoo.com.br>
2019-07-17 21:24:48 -0300

    Create actions and action creators to reducer shopping cart

---
+++ src/reducers/shoppingCart/actions.js
@@ -0,0 +1,4 @@
+export const ADD_ITEM_CART = 'shoppingCart:ADD_ITEM_CART'
+export const REMOVE_ITEM_CART = 'shoppingCart:REMOVE_ITEM_CART'
+export const ADD_QNTD = 'shoppingCart:ADD_QNTD'
+export const REMOVE_QNTD = 'shoppingCart:REMOVE_QNTD'
---
+++ src/reducers/shoppingCart/actionsCreators.js
@@ -0,0 +1,29 @@
+import { ADD_ITEM_CART, REMOVE_ITEM_CART, ADD_QNTD, REMOVE_QNTD } from './actions'
+
+export const addItemCart = (produto) => dispatch => {
+  dispatch({
+    type: ADD_ITEM_CART,
+    payload: produto
+  })
+}
+
+export const removeItemCart = (id) => dispatch => {
+  dispatch({
+    type: REMOVE_ITEM_CART,
+    payload: id
+  })
+}
+
+export const addQntd = id => dispatch => {
+  dispatch({
+    type: ADD_QNTD,
+    payload: id
+  })
+}
+
+export const removeQntd = id => dispatch => {
+  dispatch({
+    type: REMOVE_QNTD,
+    payload: id
+  })
+}

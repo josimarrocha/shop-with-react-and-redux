8b0f48104d9ded8d975872700dadff45634dc44d
Josimar <rocha_josimar@yahoo.com.br>
2019-07-17 21:11:23 -0300

    Create database local

---
+++ src/produtos.js
@@ -0,0 +1,165 @@
+export default {
+  produtos: [
+    {
+      "id": 1,
+      "title": "iPhone 6s 64GB",
+      "price": "R$ 1651,00",
+      "brand": "Apple",
+      "inCart": false,
+      "image": "images/apple-iphone6s.png",
+      "imageBig": "images/images-big/apple-iphone6s.png",
+      "description": "O Apple iPhone 6S é um smartphone iOS de bom nível, ótimo para fotos, que pode satisfazer até o mai exigente dos usuários. Tem uma tela Touchscreen de 4.7 polegadas com uma resolução de 1334x750 pixels. Sobre, características deste Apple iPhone 6S na verdade não falta nada. Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet, além de conectividade Wi-fi e GPS. Tem também leitor multimídia, videoconferência e bluetooth. Enfatizamos a boa memória interna de 128 GB mas sem a possibilidade de expansão. O Apple iPhone 6S é um produto com poucos concorrentes em termos de multimídia graças à câmera de 12 megapixels que permite ao Apple iPhone 6S tirar fotos de alta qualidade com uma resolução de 4608x2592 pixels e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels. Muito fino, 7.1 milímetros, o que torna o Apple iPhone 6S realmente interessante."
+    },
+    {
+      "id": 2,
+      "title": "iPhone X 256GB",
+      "price": "R$ 4299,00",
+      "brand": "Apple",
+      "inCart": false,
+      "image": "images/apple-iphonex.png",
+      "imageBig": "images/images-big/apple-iphonex.png",
+      "description": "O Apple iPhone X é um dos smartphones iOS mais avançados e completos que existem em circulação.Tem um grande display de 5.8 polegadas com uma resolução de 2436x1125 pixel. As funcionalidades oferecidas pelo Apple iPhone X são muitas e inovadoras. Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet. Enfatizamos a excelente memória interna de 256 GB mas sem a possibilidade de expansão. Câmera discreta de 12 megapixel mas que permite ao Apple iPhone X tirar fotos de boa qualidade com uma resolução de 4608x2592 pixel e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels. A espessura de 7.7mm torna o Apple iPhone X um dos telefones mais completos e finos."
+    },
+    {
+      "id": 3,
+      "title": "LG G7 ThinQ 64GB",
+      "price": "R$ 1699,00",
+      "brand": "LG",
+      "inCart": false,
+      "image": "images/lg-g7.png",
+      "imageBig": "images/images-big/lg-g7.png",
+      "description": "O LG G7 ThinQ é um smartphone Android com características inovadoras que o tornam uma excelente opção para qualquer tipo de utilização, representando um dos melhores dispositivos móveis já feitos.A tela de 6.1 polegadas coloca esse LG no topo de sua categoria. Além disso a resolução é das mais altas atualmente em circulação: 3120 x 1440 pixel.As funcionalidades oferecidas pelo LG G7 ThinQ são muitas e todas top de linha.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet, além de conectividade Wi- fi e GPS presente no aparelho.Tem também leitor multimídia, rádio, videoconferência, e bluetooth.Enfatizamos a excelente memória interna de 64 GB com a possibilidade de expansão. A excelência deste LG G7 ThinQ é completada por uma câmera de 16 megapixels que permite tirar fotos fantásticas com uma resolução de 4640 x 3480 pixels e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels.A espessura de apenas 7.9 milímetros torna o LG G7 ThinQ um dos telefones mais finos que existem."
+    },
+    {
+      "id": 4,
+      "title": "LG Q7 64GB",
+      "price": "R$ 1259,00",
+      "brand": "LG",
+      "inCart": false,
+      "image": "images/lg-q7.png",
+      "imageBig": "images/images-big/lg-q7.png",
+      "description": "O LG Q7 é um smartphone Android completo, que não tem muito a invejar aos mais avançados dispositivos. Tem uma enorme tela Touchscreen de 5.5 polegadas com uma resolução de 2160x1080 pixel.Quanto às funções, no LG Q7 realmente não falta nada.Começando pelo conectividade Wi- fi e GPS.A transferência de dados e navegação web sao fornecidas pela rede UMTS, mas não suporta tecnologias mais recentes, tais como HSDPA.Enfatizamos a boa memória interna de 64 GB com a possibilidade de expansão. Este LG Q7 é um produto com poucos concorrentes em termos de multimídia graças à câmera de 13 megapixels que permite ao LG Q7 tirar fotos fantásticas com uma resolução de 4163x3122 pixels e gravar vídeos em alta definição Full HD com uma resolução de 1920x1080 pixels.Muito fino, 8.4 milímetros, o que torna o LG Q7 realmente interessante."
+    },
+    {
+      "id": 5,
+      "title": "Motorola Moto G7 64GB",
+      "price": "R$ 987,00",
+      "brand": "Motorola",
+      "inCart": false,
+      "image": "images/motorola-moto-g7.png",
+      "imageBig": "images/images-big/motorola-moto-g7.png",
+      "description": "O Motorola Moto G7 é um smartphone Android avançado e abrangente em todos os pontos de vista com algumas características excelentes.Tem uma grande tela de 6.24 polegadas com uma resolução de 2270 x 1080 pixels.As funcionalidades oferecidas pelo Motorola Moto G7 são muitas e inovadoras.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet.Enfatizamos a excelente memória interna de 64 GB com a possibilidade de expansão. Respeitável a câmera de 12 megapixels que permite ao Motorola Moto G7 tirar fotos com uma resolução de 4000x3000 pixels e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels.Muito fino, 7.9 milímetros, o que torna o Motorola Moto G7 realmente interessante."
+    },
+    {
+      "id": 6,
+      "title": "Motorola Moto G7 Plus 64GB",
+      "price": "R$ 1157,00",
+      "brand": "Motorola",
+      "inCart": false,
+      "image": "images/motorola-moto-g7-plus.png",
+      "imageBig": "images/images-big/motorola-moto-g7-plus.png",
+      "description": "O Motorola Moto G7 Plus é um smartphone Android avançado e abrangente em todos os pontos de vista com algumas características excelentes.Tem uma grande tela de 6.24 polegadas com uma resolução de 2270 x 1080 pixels.As funcionalidades oferecidas pelo Motorola Moto G7 Plus são muitas e inovadoras.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet.Enfatizamos a excelente memória interna de 64 GB com a possibilidade de expansão. O Motorola Moto G7 Plus é um produto com poucos concorrentes em termos de multimídia graças à câmera de 16 megapixels que permite ao Motorola Moto G7 Plus tirar fotos fantásticas com uma resolução de 4619x3464 pixels e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels.Muito fino, 8.3 milímetros, o que torna o Motorola Moto G7 Plus realmente interessante."
+    },
+    {
+      "id": 7,
+      "title": "Motorola Moto X Play 32GB",
+      "price": "R$ 944,00",
+      "brand": "Motorola",
+      "inCart": false,
+      "image": "images/motorola-motox-play.png",
+      "imageBig": "images/images-big/motorola-motox-play.png",
+      "description": "O Motorola Moto X Play é um smartphone Android de bom nível, ótimo para fotos, que pode satisfazer até o mais exigente dos usuários.Tem uma grande tela Touchscreen de 5.5 polegadas com uma boa resolução de 1920x1080 pixels.Sobre as características deste Motorola Moto X Play na verdade não falta nada.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet, além de conectividade Wi- fi e GPS.Tem também leitor multimídia, rádio, videoconferência e bluetooth.Enfatizamos a boa memória interna de 32 GB com a possibilidade de expansão. O Motorola Moto X Play é um produto com poucos concorrentes em termos de multimídia graças à câmera de 21 megapixels que permite ao Motorola Moto X Play tirar fotos fantásticas com uma resolução de 5291x3968 pixels e gravar vídeos em alta definição Full HD com uma resolução de 1920x1080 pixels."
+    },
+    {
+      "id": 8,
+      "title": "Samsung Galaxy S8 64GB",
+      "price": "R$ 1999,00",
+      "brand": "Samsung",
+      "inCart": false,
+      "image": "images/samsung-s8.png",
+      "imageBig": "images/images-big/samsung-s8.png",
+      "description": "O Samsung Galaxy S8 é um dos smartphones Android mais avançados e completos que existem em circulação. Tem um grande display de 5.8 polegadas e uma resolução de 2960x1440 pixels que é uma das mais altas atualmente em circulação.As funcionalidades oferecidas pelo Samsung Galaxy S8 são muitas e inovadoras.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet.Enfatizamos a excelente memória interna de 64 GB com a possibilidade de expansão. Câmera discreta de 12 megapixel mas que permite ao Samsung Galaxy S8 tirar fotos de boa qualidade com uma resolução de 4290x2800 pixel e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels.A espessura de 8mm torna o Samsung Galaxy S8 um dos telefones mais completos e finos."
+    },
+    {
+      "id": 9,
+      "title": "Samsung Galaxy S9 128GB",
+      "price": "R$ 2249,00",
+      "brand": "Samsung",
+      "inCart": false,
+      "image": "images/samsung-s9.png",
+      "imageBig": "images/images-big/samsung-s9.png",
+      "description": "O Samsung Galaxy S9 é um dos smartphones Android mais avançados e completos que existem em circulação. Tem um grande display de 5.8 polegadas e uma resolução de 2960x1440 pixels que é uma das mais altas atualmente em circulação.As funcionalidades oferecidas pelo Samsung Galaxy S9 são muitas e inovadoras.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet.Enfatizamos a excelente memória interna de 128 GB com a possibilidade de expansão. Câmera discreta de 12 megapixel mas que permite ao Samsung Galaxy S9 tirar fotos de boa qualidade com uma resolução de 4032x3024 pixel e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels. "
+    },
+    {
+      "id": 10,
+      "title": "Sony Xperia C5 Ultra 32GB",
+      "price": "R$ 1300,00",
+      "brand": "Sony",
+      "inCart": false,
+      "image": "images/sony-xperia-c5.png",
+      "imageBig": "images/images-big/sony-xperia-c5.png",
+      "description": "O Sony Xperia C5 Ultra é um smartphone Android completo, que não tem muito a invejar aos mais avançados dispositivos. Surpreendente é sua tela Touchscreen de 6 polegadas, que coloca esse Sony no topo de sua categoria.A resolução também é alta: 1920x1080 pixel.Quanto às funções, no Sony Xperia C5 Ultra realmente não falta nada.Começando pelo conectividade Wi- fi e GPS.A transferência de dados e navegação web sao fornecidas pela rede UMTS, mas não suporta tecnologias mais recentes, tais como HSDPA.Enfatizamos a memória interna de 16 GB com a possibilidade de expansão. Este Sony Xperia C5 Ultra é um produto com poucos concorrentes em termos de multimídia graças à câmera de 13 megapixels que permite ao Sony Xperia C5 Ultra tirar fotos fantásticas com uma resolução de 4128x3096 pixels e gravar vídeos em alta definição Full HD com uma resolução de 1920x1080 pixels.Muito fino, 8.2 milímetros, o que torna o Sony Xperia C5 Ultra realmente interessante. "
+    },
+    {
+      "id": 11,
+      "title": "Sony Xperia Z5 dourado 64GB",
+      "price": "R$ 1400,00",
+      "brand": "Sony",
+      "inCart": false,
+      "image": "images/sony-xperia-z5.png",
+      "imageBig": "images/images-big/sony-xperia-z5.png",
+      "description": "O Sony Xperia Z5 é um smartphone Android de bom nível, ótimo para fotos, que pode satisfazer até o mais exigente dos usuários.Tem uma grande tela Touchscreen de 5.2 polegadas com uma boa resolução de 1920x1080 pixels.Sobre as características deste Sony Xperia Z5 na verdade não falta nada.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet, além de conectividade Wi- fi e GPS.Tem também leitor multimídia, rádio, videoconferência e bluetooth.Enfatizamos a boa memória interna de 32 GB com a possibilidade de expansão. O Sony Xperia Z5 é um produto com poucos concorrentes em termos de multimídia graças à câmera de 23 megapixels que permite ao Sony Xperia Z5 tirar fotos fantásticas com uma resolução de 5520x4140 pixels e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels.Muito fino, 7.3 milímetros, o que torna o Sony Xperia Z5 realmente interessante. "
+    },
+    {
+      "id": 12,
+      "title": "Xiaomi Mi 6 128GB",
+      "price": "R$ 1100,00",
+      "brand": "Xiaomi",
+      "inCart": false,
+      "image": "images/xiaomi-mi6.png",
+      "imageBig": "images/images-big/xiaomi-mi6.png",
+      "description": "O Xiaomi Mi 6 é um smartphone Android avançado e abrangente em todos os pontos de vista com algumas características excelentes.Tem uma grande tela de 5.15 polegadas com uma resolução de 1920x1080 pixels.As funcionalidades oferecidas pelo Xiaomi Mi 6 são muitas e inovadoras.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet.Enfatizamos a excelente memória interna de 128 GB mas sem a possibilidade de expansão. Respeitável a câmera de 12 megapixels que permite ao Xiaomi Mi 6 tirar fotos com uma resolução de 4032x3024 pixels e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels.Muito fino, 7.5 milímetros, o que torna o Xiaomi Mi 6 realmente interessante. "
+    },
+    {
+      "id": 13,
+      "title": "Xiaomi Redmi 5 16GB",
+      "price": "R$ 900,00",
+      "brand": "Xiaomi",
+      "inCart": false,
+      "image": "images/xiaomi-redmi-plus.png",
+      "imageBig": "images/images-big/xiaomi-redmi-plus.png",
+      "description": "O Xiaomi Redmi 5 é um smartphone Android completo, que não tem muito a invejar aos mais avançados dispositivos. Tem uma enorme tela Touchscreen de 5.7 polegadas com uma resolução de 1440x720 pixel.Quanto às funções, no Xiaomi Redmi 5 realmente não falta nada.Começando pelo conectividade Wi- fi e GPS.A transferência de dados e navegação web sao fornecidas pela rede UMTS, mas não suporta tecnologias mais recentes, tais como HSDPA.Enfatizamos a memória interna de 16 GB com a possibilidade de expansão. Este Xiaomi Redmi 5 é um produto com poucos concorrentes em termos de multimídia graças à câmera de 12 megapixels que permite ao Xiaomi Redmi 5 tirar fotos fantásticas com uma resolução de 4000x3000 pixels e gravar vídeos em alta definição Full HD com uma resolução de 1920x1080 pixels.Muito fino, 7.7 milímetros, o que torna o Xiaomi Redmi 5 realmente interessante. "
+    },
+    {
+      "id": 14,
+      "title": "Xiaomi Redmi Note 7 32GB",
+      "price": "R$ 1099,00",
+      "brand": "Xiaomi",
+      "inCart": false,
+      "image": "images/xiaomi-redmi-note.png",
+      "imageBig": "images/images-big/xiaomi-redmi-note.png",
+      "description": "O Redmi Note 7 é, sem dúvida, um dos smartphones Android mais avançados e abrangentes disponíveis no mercado, graças ao seu rico equipamento e recursos multimídia avançados.Tem um grande display de 6.3 polegadas com uma resolução de 2340x1080 pixel.As funcionalidades oferecidas pelo Redmi Note 7 são muitas e inovadoras.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet.Enfatizamos a memória interna de 32 GB com a possibilidade de expansão. O Redmi Note 7 é um produto com poucos concorrentes em termos de multimídia graças à câmera de 48 megapixels que permite ao Redmi Note 7 tirar fotos fantásticas com uma resolução de 8000x6000 pixels e gravar vídeos em alta definição Full HD com uma resolução de 1920x1080 pixels. "
+    },
+    {
+      "id": 15,
+      "title": "Asus ZenFone 5 64GB",
+      "price": "R$ 1320,00",
+      "brand": "Asus",
+      "inCart": false,
+      "image": "images/asus-zenfone5.png",
+      "imageBig": "images/images-big/asus-zenfone5.png",
+      "description": "O Asus ZenFone 5 é um smartphone Android avançado e abrangente em todos os pontos de vista com algumas características excelentes.Tem uma grande tela de 6.2 polegadas com uma resolução de 2246x1080 pixels.As funcionalidades oferecidas pelo Asus ZenFone 5 são muitas e inovadoras.Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet.Enfatizamos a excelente memória interna de 64 GB com a possibilidade de expansão. Respeitável a câmera de 12 megapixels que permite ao Asus ZenFone 5 tirar fotos com uma resolução de 4160x3120 pixels e gravar vídeos em 4K a espantosa resolução de 3840x2160 pixels.Muito fino, 7.9 milímetros, o que torna o Asus ZenFone 5 realmente interessante. "
+    },
+    {
+      "id": 16,
+      "title": "Asus ZenFone Max Plus (M2) 32GB",
+      "price": "R$ 920,00",
+      "brand": "Asus",
+      "inCart": false,
+      "image": "images/asus-zenfone-max-plus.png",
+      "imageBig": "images/images-big/asus-zenfone-max-plus.png",
+      "description": "O Asus ZenFone Max Plus M2 é um smartphone Android de bom nível, ótimo para fotos, que pode satisfazer até o mais exigente dos usuários. Tem uma enorme tela Touchscreen de 6.26 polegadas com uma resolução de 2280x1080 pixel. Sobre as características deste Asus ZenFone Max Plus M2 na verdade não falta nada. Começando pelo LTE 4G que permite a transferência de dados e excelente navegação na internet, além de conectividade Wi-fi e GPS. Tem também leitor multimídia, rádio, videoconferência e bluetooth. Enfatizamos a boa memória interna de 32 GB com a possibilidade de expansão. O Asus ZenFone Max Plus M2 é um produto com poucos concorrentes em termos de multimídia graças à câmera de 12 megapixels que permite ao Asus ZenFone Max Plus M2 tirar fotos de alta qualidade com uma resolução de 4000x3000 pixels e gravar vídeos em alta definição Full HD com uma resolução de 1920x1080 pixels.Muito fino, 8.5 milímetros, o que torna o Asus ZenFone Max Plus M2 realmente"
+    }
+  ],
+  brands: ['Apple', 'Asus', 'Samsung', 'LG', 'Xiaomi', 'Sony', 'Motorola']
+}

3902f60ed917bedfc35070e0cfacecdd76fb14b5
Josimar <rocha_josimar@yahoo.com.br>
2019-07-17 21:15:03 -0300

    Create actions to reducer products

---
+++ src/reducers/produtos/actions.js
@@ -0,0 +1,4 @@
+export const SHOW_PRODUTOS = 'products:SHOW_PRODUTOS'
+export const PRODUCTS_SEARCH_BRAND = 'products:PRODUCTS_SEARCH_BRAND'
+export const PRODUCTS_SEARCH_INPUT = 'products:PRODUCTS_SEARCH_INPUT'
+export const UPDATE_DATA = 'products:UPDATE_DATA'

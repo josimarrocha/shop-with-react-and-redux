9b07a937de16d308270d904d9e29f2353eec822e
Josimar <rocha_josimar@yahoo.com.br>
2019-07-17 21:21:01 -0300

    Create component intro

---
+++ src/components/Intro/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div className="header-intro d-flex justify-content-center">
+      <div className="header-img-itro">
+        <img src="images/smartphone-intro.jpg" className='img-fluid' alt="Smartphones Top de Linha" />
+      </div>
+    </div>
+  )
+}
+
+export default Intro

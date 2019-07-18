433908c42e6b059d23077c27969cdcfa889bb7f8
Josimar <rocha_josimar@yahoo.com.br>
2019-07-17 21:20:21 -0300

    Create component list product and styles

---
+++ src/components/ListProdutos/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import { updateData } from '../../reducers/produtos/actionsCreators'
+import { addItemCart } from '../../reducers/shoppingCart/actionsCreators'
+import Produto from './Produto'
+
+import './styles.css'
+
+const ListProdutos = ({ produtos, addProduct, updatedb }) => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    setProducts(produtos)
+  }, [produtos, products])
+
+  return (
+    <div className="col-lg-10 col-md-12 col-sm-12 mt-2">
+      <div className="products">
+        <div className="row">
+          {products && products.map(produto => (
+            <Produto
+              key={`produto:${produto.id}`}
+              produto={produto}
+              addItemCart={addProduct}
+              updateDB={updatedb}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+const mapStateToProps = state => ({
+  produtos: state.produtos.produtos,
+  productsSearch: state.produtos.productsSearchBrand,
+  cart: state.shoppingCart
+})
+
+export default connect(mapStateToProps, { addProduct: addItemCart, updatedb: updateData })(ListProdutos)

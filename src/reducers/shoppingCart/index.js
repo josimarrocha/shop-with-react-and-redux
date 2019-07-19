00f870f663899f3e7fa92fed1b9ce485d49ac2be
Josimar <rocha_josimar@yahoo.com.br>
2019-07-19 20:38:50 -0300

    Update add and remove products of local storage

--- src/reducers/shoppingCart/index.js
+++ src/reducers/shoppingCart/index.js
@@ -1,19 +1,28 @@
-import { ADD_ITEM_CART, REMOVE_ITEM_CART, ADD_QNTD, REMOVE_QNTD } from './actions'
+import { ADD_ITEM_CART, REMOVE_ITEM_CART, ADD_QNTD, REMOVE_QNTD, LOADING_PRODUCTS_STORAGE } from './actions'
 
 const initialState = {
   cart: []
 }
 const shoppingCart = (state = initialState, action) => {
+  let products
   switch (action.type) {
+    case LOADING_PRODUCTS_STORAGE:
+      return {
+        cart: action.payload
+      }
     case ADD_ITEM_CART:
+      products = state.cart.concat({ ...action.payload, inCart: true, qntd: 1 })
+      localStorage.setItem('@cartProducts', JSON.stringify({ products }))
       return {
         ...state,
-        cart: state.cart.concat({ ...action.payload, inCart: true, qntd: 1 })
+        cart: products
       }
     case REMOVE_ITEM_CART:
+      products = state.cart.filter(itemCart => itemCart.id !== action.payload)
+      localStorage.setItem('@cartProducts', JSON.stringify({ products }))
       return {
         ...state,
-        cart: state.cart.filter(itemCart => itemCart.id !== action.payload)
+        cart: products
       }
     case ADD_QNTD:
       return {

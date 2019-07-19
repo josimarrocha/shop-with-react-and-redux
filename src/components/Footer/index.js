81e507c4f4938b8863bcd74d6e2a36bedee14ffd
Josimar <rocha_josimar@yahoo.com.br>
2019-07-19 13:29:45 -0300

    Create component footer and atyles

---
+++ src/components/Footer/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './styles.css'
+
+const Footer = () => {
+  return (
+    <section className='footer mt-5 px-2' >
+      <footer className="container-fluid py-4 px-md-5 px-sm-3">
+        <div className="row">
+          <div className="social-media col-12 col-sm-4 col-md-4 ml-sm-2">
+            <h5 className='text-uppercase'>Redes socias</h5>
+            <div className="facebook mt-3">
+              <a href="">
+                <i className="fab fa-facebook-f mr-2 px-2 pt-2" />
+                <span>Facebook</span>
+              </a>
+            </div>
+            <div className="github mt-2">
+              <a href="">
+                <i className="fab fa-github mr-2 px-2 pt-2" />
+                <span>GitHub</span>
+              </a>
+            </div>
+            <div className="linkedin mt-2">
+              <a href="">
+
+                <i className="fab fa-linkedin-in mr-2 px-2 pt-2" />
+                <span>Linkedin</span>
+              </a>
+            </div>
+          </div>
+          <div className="contatos col-12 col-sm-6 col-md-5 ml-sm-4 mt-2 mt-sm-0 mt-md-0">
+            <h5 className='text-uppercase'>Contatos</h5>
+            <p className='mt-3'>
+              <span className='mr-1'>Email:</span>
+              josimarsantosdarocha@gmail.com
+              </p>
+            <p>
+              <span className='mr-1'>Telefone:</span>
+              11 - 9999-9999
+              </p>
+            <p>
+              <span className='mr-1'>Endereço:</span>
+              Lorem ipsum dolor sit amet, 58, SP
+            </p>
+          </div>
+
+        </div>
+      </footer>
+    </section>
+  )
+}
+
+export default Footer

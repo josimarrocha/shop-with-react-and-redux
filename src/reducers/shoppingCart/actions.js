e5ce364615d6aaf387f1e4e1a1531a9cebbe7a4d
Josimar <rocha_josimar@yahoo.com.br>
2019-07-19 20:34:39 -0300

    Update add action

--- src/reducers/shoppingCart/actions.js
+++ src/reducers/shoppingCart/actions.js
@@ -1,3 +1,4 @@
+export const LOADING_PRODUCTS_STORAGE = 'shoppingCart:LOADING_PRODUCTS_STORAGE'
 export const ADD_ITEM_CART = 'shoppingCart:ADD_ITEM_CART'
 export const REMOVE_ITEM_CART = 'shoppingCart:REMOVE_ITEM_CART'
 export const ADD_QNTD = 'shoppingCart:ADD_QNTD'

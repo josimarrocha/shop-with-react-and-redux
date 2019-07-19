9f5a646faf29fd43c805b4945c95610325b4022d
Josimar <rocha_josimar@yahoo.com.br>
2019-07-19 14:02:36 -0300

    Add carousel in reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import produtos from './produtos'
 import ui from './ui'
 import shoppingCart from './shoppingCart'
+import carossel from './caroussel'
 
 export default combineReducers({
   produtos,
   ui,
-  shoppingCart
+  shoppingCart,
+  carossel
 })

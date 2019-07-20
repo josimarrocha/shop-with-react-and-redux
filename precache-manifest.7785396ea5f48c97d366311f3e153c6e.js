a68dfc93d7c410eb63841b05cef642e1bae38619
josimarrocha <rocha_josimar@yahoo.com.br>
2019-07-20 01:28:08 +0000

    Updates

---
+++ precache-manifest.7785396ea5f48c97d366311f3e153c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0978ce3824dd48ade1e9409282ced72b",
+    "url": "/shop-with-react-and-redux/index.html"
+  },
+  {
+    "revision": "306058a33ff5e1561fb3",
+    "url": "/shop-with-react-and-redux/static/css/2.e904a037.chunk.css"
+  },
+  {
+    "revision": "ec9d1fde98f8cc39e514",
+    "url": "/shop-with-react-and-redux/static/css/main.36b0f2ce.chunk.css"
+  },
+  {
+    "revision": "306058a33ff5e1561fb3",
+    "url": "/shop-with-react-and-redux/static/js/2.f8c15369.chunk.js"
+  },
+  {
+    "revision": "ec9d1fde98f8cc39e514",
+    "url": "/shop-with-react-and-redux/static/js/main.b8626f59.chunk.js"
+  },
+  {
+    "revision": "7cc9cef185d3b79fd57a",
+    "url": "/shop-with-react-and-redux/static/js/runtime~main.cfe0a161.js"
+  }
+]);
